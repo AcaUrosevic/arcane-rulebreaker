53f4a659d57452dd4ce2cedae4e6b8d798d804e2
AcaUrosevic <aleksandar.urosevic@fonis.rs>
2025-08-07 00:13:33 +0200

    Initial project setup: scene, top-down camera, movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody rb;
+    private Vector3 moveDirection;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveZ = Input.GetAxisRaw("Vertical");
+        moveDirection = new Vector3(moveX, 0f, moveZ).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveDirection * moveSpeed * Time.fixedDeltaTime);
+    }
+}
